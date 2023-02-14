19fac89d533a47c477a8ad23fdc3bd6d8fc11ffb
Rihan <me@file.properties>
2023-02-14 04:38:35 +0000

    Create components

---
+++ components/Button.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+const props = withDefaults(
+  defineProps<{
+    type?: "button" | "a";
+    block?: boolean;
+  }>(),
+  {
+    type: "button",
+  }
+);
+</script>
+
+<template>
+  <button
+    v-if="type === 'button'"
+    type="button"
+    class="rounded-full bg-neutral-900 py-3 px-7 font-semibold text-white transition-colors hover:bg-neutral-700 dark:bg-neutral-100 dark:text-black dark:hover:bg-neutral-300"
+    :class="{ 'w-full': block }"
+  >
+    <slot />
+  </button>
+
+  <NuxtLink
+    v-else
+    class="block rounded-full bg-neutral-900 py-3 px-7 font-semibold text-white transition-colors hover:bg-neutral-700 dark:bg-neutral-100 dark:text-black dark:hover:bg-neutral-300"
+    :class="{ 'w-full': block }"
+  >
+    <slot />
+  </NuxtLink>
+</template>
---
+++ components/Card.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="rounded-2xl bg-neutral-200 p-5 dark:bg-neutral-800">
+    <slot />
+  </div>
+</template>
