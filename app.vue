3812ee2b9561074224e3c78a9005cc2d05e865f7
Rihan <me@file.properties>
2023-02-14 04:38:04 +0000

    Add font

--- app.vue
+++ app.vue
@@ -1,5 +1,24 @@
-<template>
-  <div>
-    <NuxtWelcome />
-  </div>
+<script setup lang="ts">
+useHead({
+  titleTemplate: "%s | SRVControl",
+});
+</script>
+
+<template class="min-h-screen">
+  <Head>
+    <Link rel="icon" type="image/png" href="/logo.png" />
+    <Link rel="preconnect" href="https://fonts.googleapis.com" />
+    <Link
+      rel="preconnect"
+      href="https://fonts.gstatic.com"
+      crossorigin="anonymous"
+    />
+    <Link
+      href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap"
+      rel="stylesheet"
+    />
+  </Head>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -9,7 +11,11 @@ module.exports = {
     "./app.vue",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Roboto", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
   darkMode: "class",
