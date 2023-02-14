704f7c79fd5539f8ed64fb7a23775afcb2e69695
Rihan <me@file.properties>
2023-02-14 04:38:20 +0000

    Create layout

--- app.vue
+++ app.vue
@@ -2,6 +2,9 @@
 useHead({
   titleTemplate: "%s | SRVControl",
 });
+
+const isDark = useDark();
+const toggleDark = useToggle(isDark);
 </script>
 
 <template class="min-h-screen">
@@ -17,8 +20,24 @@ useHead({
       href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap"
       rel="stylesheet"
     />
+    <Body
+      class="mx-auto min-h-screen max-w-lg bg-neutral-100 px-3 transition-colors dark:bg-neutral-900 dark:text-neutral-300"
+    />
   </Head>
-  <NuxtLayout>
-    <NuxtPage />
-  </NuxtLayout>
+  <div class="flex min-h-screen flex-col">
+    <Header />
+    <div class="flex-1">
+      <NuxtLayout>
+        <NuxtPage />
+      </NuxtLayout>
+    </div>
+    <div class="my-5 flex justify-center">
+      <button
+        class="h-10 w-10 rounded-xl border border-neutral-400 p-2 dark:border-neutral-600"
+        @click="toggleDark()"
+      >
+        {{ isDark ? "☀️" : "🌙" }}
+      </button>
+    </div>
+  </div>
 </template>
