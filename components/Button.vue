3d2f9645931d5b716c8b63a2804ddfb77873f7ac
Rihan <me@file.properties>
2023-02-19 19:14:39 +0000

    Style active state

--- components/Button.vue
+++ components/Button.vue
@@ -14,7 +14,7 @@ const props = withDefaults(
   <button
     v-if="type === 'button'"
     type="button"
-    class="rounded-full bg-neutral-900 py-3 px-7 font-semibold text-white transition-colors hover:bg-neutral-700 dark:bg-neutral-100 dark:text-black dark:hover:bg-neutral-300"
+    class="rounded-full bg-neutral-900 py-3 px-7 font-semibold text-white transition-colors hover:bg-neutral-700 active:bg-neutral-600 dark:bg-neutral-100 dark:text-black dark:hover:bg-neutral-300 dark:active:active:bg-neutral-400"
     :class="{ 'w-full': block }"
   >
     <slot />
@@ -22,7 +22,7 @@ const props = withDefaults(
 
   <NuxtLink
     v-else
-    class="block rounded-full bg-neutral-900 py-3 px-7 font-semibold text-white transition-colors hover:bg-neutral-700 dark:bg-neutral-100 dark:text-black dark:hover:bg-neutral-300"
+    class="block rounded-full bg-neutral-900 py-3 px-7 font-semibold text-white transition-colors hover:bg-neutral-700 active:bg-neutral-600 dark:bg-neutral-100 dark:text-black dark:hover:bg-neutral-300 dark:active:active:bg-neutral-400"
     :class="{ 'w-full': block }"
   >
     <slot />
