c303ac1cf9e12a1c6fb2609005eb256debceda1f
Rihan <me@file.properties>
2023-02-19 20:51:37 +0000

    Move links to useHead

--- app.vue
+++ app.vue
@@ -1,22 +1,24 @@
 <script setup lang="ts">
 useHead({
   titleTemplate: "%s | SRVControl",
+  link: [
+    { rel: "icon", type: "image/png", href: "/logo.png" },
+    { rel: "preconnect", href: "https://fonts.googleapis.com" },
+    {
+      rel: "preconnect",
+      href: "https://fonts.gstatic.com",
+      crossorigin: "anonymous",
+    },
+    {
+      rel: "stylesheet",
+      href: "https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap",
+    },
+  ],
 });
 </script>
 
 <template class="min-h-screen">
   <Head>
-    <Link rel="icon" type="image/png" href="/logo.png" />
-    <Link rel="preconnect" href="https://fonts.googleapis.com" />
-    <Link
-      rel="preconnect"
-      href="https://fonts.gstatic.com"
-      crossorigin="anonymous"
-    />
-    <Link
-      href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap"
-      rel="stylesheet"
-    />
     <Body
       class="mx-auto max-w-lg bg-neutral-100 px-3 transition-colors dark:bg-neutral-900 dark:text-neutral-300"
     />
