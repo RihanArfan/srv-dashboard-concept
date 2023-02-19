3b63f0e88defd836687c13fac93fa7e8396249a0
Rihan <me@file.properties>
2023-02-19 19:14:15 +0000

    Add icons for social links in header

--- components/Header.vue
+++ components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="my-7 flex justify-between gap-2 align-middle">
+  <div class="my-7 flex items-center justify-between">
     <NuxtLink
       to="/"
       class="flex gap-2 rounded-xl p-2 transition-colors hover:bg-neutral-200 dark:hover:bg-neutral-800"
@@ -7,19 +7,17 @@
       <img src="/logo.png" alt="SRVControl Logo" class="h-8 w-8 rounded-full" />
       <p class="my-auto text-xl font-semibold">SRVControl</p>
     </NuxtLink>
-    <div class="my-auto flex gap-2 pr-2">
+    <div class="flex items-center gap-3 pr-2">
       <NuxtLink
         to="https://twitter.com/SRVControl"
-        class="h-10 w-10 rounded-full bg-neutral-200 dark:bg-neutral-700"
         target="_blank"
         title="Twitter"
-      />
-      <NuxtLink
-        to="https://discord.gg/7d2s79t"
-        class="h-10 w-10 rounded-full bg-neutral-200 dark:bg-neutral-700"
-        target="_blank"
-        title="Discord"
-      />
+      >
+        <NavIcon><Icon name="fa6-brands:twitter" size="1.1em" /></NavIcon>
+      </NuxtLink>
+      <NuxtLink to="https://discord.gg/7d2s79t" target="_blank" title="Discord">
+        <NavIcon><Icon name="fa6-brands:discord" size="1.3em" /></NavIcon>
+      </NuxtLink>
       <img
         src="https://cdn.discordapp.com/avatars/207629082257653760/5907d628a71a80971db000fceecaab89.png?size=4096"
         alt=""
---
+++ components/NavIcon.vue
@@ -0,0 +1,7 @@
+<template>
+  <div
+    class="flex h-10 w-10 items-center justify-center rounded-full bg-neutral-200 text-center transition-[color,transform] hover:scale-105 hover:bg-neutral-300 active:scale-110 active:bg-neutral-400 dark:bg-neutral-700 dark:hover:bg-neutral-600 dark:active:bg-neutral-500"
+  >
+    <slot />
+  </div>
+</template>
